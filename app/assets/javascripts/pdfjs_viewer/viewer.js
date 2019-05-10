fcb3e64d33d8851c05ca9568c2069ae0a3fd9146
Christoph Rahles <christoph@rahles.de>
2019-05-10 20:39:24 +0200

    allow custom origins

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -1886,7 +1886,11 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = [
+    'null', 'http://mozilla.github.io', 'https://mozilla.github.io',
+    'https://invoicechecker.s3-eu-central-1.amazonaws.com',
+    'https://invoice-checker-staging.s3-eu-central-1.amazonaws.com',
+  ];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
